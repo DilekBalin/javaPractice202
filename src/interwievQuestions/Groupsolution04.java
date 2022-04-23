1c3eb4a5bdd32a8644fa14af85f14b5e380e6968
DilekBalin <dilekkara1231@gmail.com>
2022-04-23 15:35:54 +0100

    pin sorusu try catch

--- src/interwievQuestions/Groupsolution04.java
+++ src/interwievQuestions/Groupsolution04.java
@@ -1,7 +1,43 @@
 package interwievQuestions;
 
+import java.util.Scanner;
+
 public class Groupsolution04 {
     public static void main(String[] args) {
+        // STRING OLAN PIN kodunuzu kontrol eden bir kod yazınız.
+            giris();
+        }
+        public static void giris() {
+            Scanner scan = new Scanner(System.in);
+            int pin = 0;
+            while (true) {
+                try {
+                    System.out.println("pin giriniz");
+                    pin = scan.nextInt();
+                    break;
+                } catch (Exception e) {
+                    String str = scan.nextLine();
+                    System.out.println("numerik value gir");
+                }
+            }
+            int dogruPin =1234;
+            int girisHakki = 3;
+            while (true) {
+                if (pin == (dogruPin)) {
+                    System.out.println("dogru pin");
+                    break;
+                } else {
+                    scan.nextLine();
+                    System.out.println("yanlis giris");
+                    girisHakki--;
+                    System.out.println("kalan hak : " + girisHakki);
+                    giris();
+                }
+                if (girisHakki == 0) {
+                    System.out.println("giris hakki doldu");
+                    break;
+                }
+            }
 
     }
 }
